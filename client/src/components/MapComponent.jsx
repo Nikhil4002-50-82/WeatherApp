b37a5ed1e3099cf418785f37f1ec9a25af6c9c77
Nikhil <nikhil.4002.50.82@gmail.com>
2024-11-05 21:57:43 +0530

    Update MapComponent.jsx

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { LatitudeContext } from './context/LatitudeContext';
 import { LongitudeContext } from './context/LongitudeContext';
